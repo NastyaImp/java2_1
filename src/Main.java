728a820b3957592d0fb286d4991d3d13ee38015b
NastyaImp <Imperiian@yandex.ru>
2021-10-28 21:13:28 +0300

    add commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 1006;
+        int amount = 20;
+
+        int bonus = price / amount;
+        System.out.println(bonus);
+    }
+}
